52c213945c22d026c167e8d8419ae14ab9076ed6
sthaarwin <sthaarwin123@gmail.com>
2024-11-02 08:38:16 +0545

    script fix

--- dungeon-runner/scripts/game.gd
+++ dungeon-runner/scripts/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 @onready var player = $player
+@onready var Camera = $Camera2D
+@onready var StaticBody = $StaticBody2D
 
 const GAME_START_POSITION := Vector2i(150, 485)
 const CAM_START_POSITION := Vector2i(576, 324)
@@ -16,8 +18,8 @@ func _ready():
 	score = 5
 	player.position = GAME_START_POSITION
 	player.velocity = Vector2i(0,0)
-	$Camera2D.position = CAM_START_POSITION
-	$StaticBody2D.position = Vector2i(0,0)
+	Camera.position = CAM_START_POSITION
+	StaticBody.position = Vector2i(0,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,5 +27,5 @@ func _process(delta):
 	score += speed
 	speed = START_SPEED
 	player.position.x += speed
-	$Camera2D.position.x += speed
+	Camera.position.x += speed
 	speed = START_SPEED*score/SPEED_MODIFIER
