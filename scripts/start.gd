f681ba28b23584bc8089e08f03be499e3f9e5839
Utsav Adhikari <158271037+Brook07@users.noreply.github.com>
2024-12-11 20:51:51 +0545

    added scripts for connecting the scenes

---
+++ scripts/start.gd
@@ -0,0 +1,6 @@
+extends Button
+
+func _on_Start_pressed():
+	# Ensure the path is correct for the levels scene
+	get_tree().change_scene_to_file("res://scenes/levels.tscn")
+	print("Start button pressed")
