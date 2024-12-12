eaa6c2b30edc17069486b266cb9e03cbc9aff300
Axzyte <sthaarwin123@gmail.com>
2024-12-12 11:39:10 +0545

    minor bug fixed

--- scripts/start.gd
+++ scripts/start.gd
@@ -3,4 +3,4 @@ extends Button
 func _on_Start_pressed():
 	# Ensure the path is correct for the levels scene
 	get_tree().change_scene_to_file("res://scenes/levels.tscn")
-	print("Start button pressed")
+	#print("Start button pressed")
